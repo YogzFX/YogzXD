6ae705b3c5e1e037414be4753132b918ca273bb8
YogzFX <110335811+YogzFX@users.noreply.github.com>
2022-08-01 04:14:43 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,18 +20,18 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
-*sᴛᴀᴛᴜs:* single 
+*ɴᴀᴍᴇ:* Sasuke
+*ᴀɢᴇ:* 17ᵗʰ
+*sᴛᴀᴛᴜs:* dh punya Shelly
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* 10 Maret
+*ᴀᴅᴅʀᴇss:* Mars
 
 *- - sᴋɪʟʟs: - -* 
-> JavaScript [89.7%]
-> Python [13.4%]
-> CSS [5.1%]
-> Html [1.2%]
+> Turu [89.7%]
+> Makan [13.4%]
+> Mandi [5.1%]
+> Berak [1.2%]
 
 📷 *Instagram:* ${sig}
 🐈 *Github:* ${sgh}
